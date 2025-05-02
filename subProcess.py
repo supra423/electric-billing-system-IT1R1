c5ccbd80931b9b33343dd489e171bdc9d47b7090
supra423 <alexusenzoroa@gmail.com>
2025-05-03 02:00:37 +0800

    made it possible to print a particular PID of a process for debugging purposes, because I might run more than one processes

--- subProcess.py
+++ subProcess.py
@@ -7,32 +7,36 @@ import psutil
 
 
 class subProcess:
-    def __init__(self, scriptName):
-        self.scriptName = scriptName
-        self.startProcess()
+    def __init__(self, scriptname):
+        self.scriptname = scriptname
+        self.startprocess()
 
-    def isProcessRunning(self):
+    def isprocessrunning(self):
         for proc in psutil.process_iter(attrs = ['pid', 'name', 'cmdline']):
 
             try:
-                if proc.info['name'] == 'python.exe' and self.scriptName in ' '.join(proc.info['cmdline']):
-                    return True
+                if proc.info['name'] == 'python.exe' and self.scriptname in ' '.join(proc.info['cmdline']):
+                    return proc.info['pid'] 
 
             except (psutil.NoSuchProcess, psutil.AccessDenied):
                 continue
 
-        return False
+        return None
 
-    def startProcess(self):
-        if not self.isProcessRunning():
-
-            if os.name == 'nt':
-                subprocess.Popen([sys.executable, self.scriptName], creationflags = subprocess.DETACHED_PROCESS)
-                print(f"{self.scriptName} is running!")
-            else:
-                #print("You are not running in windows!")
-                messagebox.showinfo("Error!", "You must be running on windows!")
-                raise Exception
+    def startprocess(self):
+        #if not self.isprocessrunning():
+        pid = self.isprocessrunning()
+        if os.name == 'nt' and pid is None:
+            process = subprocess.Popen([sys.executable, self.scriptname], creationflags = subprocess.DETACHED_PROCESS)
         
-        else:
-            print(f"{self.scriptName} is already running!")
+            print(f"{self.scriptname} is running! PID: {process.pid}")
+
+        elif os.name != 'nt':
+            #print("you are not running in windows!")
+            messagebox.showinfo("error!", "you must be running on windows!")
+            raise Exception
+    
+        elif pid:
+            print(f"{self.scriptname} is already running! PID: {pid}")
+
+
