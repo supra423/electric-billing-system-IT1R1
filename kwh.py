c49e4cbeccf40abd417710a666f37225310a9b77
supra423 <alexusenzoroa@gmail.com>
2025-05-06 01:06:49 +0800

    made it so that every user now has a unique amount of kWh added to their accounts every day, instead of all users having the same amount of kWh added to their accounts every day

--- kwh.py
+++ kwh.py
@@ -8,10 +8,14 @@ connection = sqlite3.connect('database.s3db')
 cursor = connection.cursor()
 
 def job():
-    kWh = random.randrange(10, 30)
-    cursor.execute('update accounts set kWh = kWh + (?)', (kWh,))
+    accountFetch = cursor.execute("select accountNumber, kWh from accounts").fetchall()
+
+    for row in accountFetch:
+        addKwh = random.randrange(10, 30)
+        cursor.execute("update accounts set kWh = kWh + ? where accountNumber = ?", (addKwh, row[0]))
+
     connection.commit()
-    print("Updated new kWh for all users")
+    print("Updated kWh for all users.")
 
 schedule.every().day.at("00:00").do(job)
 
